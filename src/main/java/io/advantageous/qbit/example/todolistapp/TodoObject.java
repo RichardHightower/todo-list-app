e773b20ae63ce19463eaf79de8951df065a0cd8a
fadihub <fadimaalouli@gmail.com>
2015-02-24 11:48:21 -0800

    workind with todoservice

--- src/main/java/io/advantageous/qbit/example/todolistapp/TodoObject.java
+++ src/main/java/io/advantageous/qbit/example/todolistapp/TodoObject.java
@@ -7,7 +7,7 @@ package io.advantageous.qbit.example.todolistapp;
 
 public class TodoObject {
     private final String todo;
-    //private final long time = System.currentTimeMillis();
+    private final long time = System.currentTimeMillis();
 
     public TodoObject(String todo) {
         this.todo = todo;
--- src/main/java/io/advantageous/qbit/example/todolistapp/TodoService.java
+++ src/main/java/io/advantageous/qbit/example/todolistapp/TodoService.java
@@ -3,7 +3,6 @@ package io.advantageous.qbit.example.todolistapp;
 import io.advantageous.qbit.annotation.*;
 
 
-
 /**
  * Created by rhightower on 2/10/15.
  */
@@ -12,9 +11,9 @@ import io.advantageous.qbit.annotation.*;
 public class TodoService {
 
 
-    @RequestMapping( value = "/todoo", method = RequestMethod.POST)
-    public TodoObject list(@RequestParam("itemContent") String item) {
-        return new TodoObject(item);
+    @RequestMapping("/todoo")
+    public TodoObject list() {
+        return new TodoObject("");
     }
 
 }
