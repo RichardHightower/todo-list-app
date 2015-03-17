4b509fc7ec74cadd19f3019befe570e95aa81942
fadihub <fadimaalouli@gmail.com>
2015-03-17 13:22:19 -0700

    works with version = 0.7.2

---
+++ src/main/java/io/advantageous/qbit/example/todolistapp/TodoService.java
@@ -0,0 +1,19 @@
+package io.advantageous.qbit.example.todolistapp;
+
+import io.advantageous.qbit.annotation.*;
+
+
+/**
+ * Created by rhightower on 2/10/15.
+ */
+
+@RequestMapping("/todoservice")
+public class TodoService {
+
+
+    @RequestMapping("/todoo")
+    public TodoObject list() {
+        return new TodoObject();
+    }
+
+}
